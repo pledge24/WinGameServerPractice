dc1faceb4b84b689deef561336a683ba204ea7bc
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-01-18 01:43:43 +0900

    🎉 add. Atomic Class Function

--- Server/GameServer.cpp
+++ Server/GameServer.cpp
@@ -169,58 +169,61 @@ void WorkerThreadMain(HANDLE iocpHandle)
 //    ::WSACleanup();
 //}
 
-int32 x = 0;
-int32 y = 0;
-int32 r1 = 0;
-int32 r2 = 0;
-
-volatile bool ready;
-
-void Thread_1()
-{
-    while (!ready)
-    {
-
-    }
-
-    y = 1;  // Store y
-    r1 = x; // Load x
-}
-
-void Thread_2()
-{
-    while (!ready)
-    {
-
-    }
-
-    x = 1;  // Store x
-    r2 = y; // Load y
-}
 
 int main(void)
 {
-    int32 count = 0;
+    atomic<bool> flag = false;
 
-    while (true)
+    // flag = true;
+    flag.store(true, memory_order::memory_order_seq_cst);
+
+    // bool val = flag;
+    bool val = flag.load(memory_order::memory_order_seq_cst);
+
+    // 이전 flag 값을 prev에 넣고, flag 값을 수정
     {
-        ready = false;
-        count++;
-
-        x = y = r1 = r2 = 0;
-
-        thread t1(Thread_1);
-        thread t2(Thread_2);
-
-        ready = true;
-
-        t1.join();
-        t2.join();
-
-        if (r1 == 0 && r2 == 0)
-            break;
+        // bool prev = flag;
+        // flag = true;
+        flag.exchange(true);    // 읽고 쓰기를 한번에 해준다.(atomic)
     }
 
-    cout << count << " 번만에 빠져나옴" << endl;
+    // CAS (Compare-And-Swap) 조건부 수정
+    {
+        bool expected = false;
+        bool desired = true;
+        flag.compare_exchange_strong(expected, desired);
+
+        // compare_exchange_strong 의사코드
+        //if (flag == expected)
+        //{
+        //    // expected = flag;
+        //    flag = desired;
+        //    return true;
+        //}
+        //else
+        //{
+        //    expected = flag;
+        //    return false;
+        //}
+
+        flag.compare_exchange_weak(expected, desired);
+
+        // compare_exchange_weak 의사코드
+        // Spurious Failure
+        //if (flag == expected)
+        //{
+        //    // 다른 쓰레드의 Interruption을 받아서 중간에 실패가능
+        //    if(묘한 상황)
+        //        return false;
+        //    // expected = flag;
+        //    flag = desired;
+        //    return true;
+        //}
+        //else
+        //{
+        //    expected = flag;
+        //    return false;
+        //}
+    }
 }
  
