92a993b93f4f852cbd21c0babc550c14851c1800
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-01-17 22:08:20 +0900

    🎉 add. Condition variable based Lock

--- Server/GameServer.cpp
+++ Server/GameServer.cpp
@@ -171,19 +171,24 @@ void WorkerThreadMain(HANDLE iocpHandle)
 
 mutex m;
 queue<int32> q;
-HANDLE handle;
+
+// 참고) CV는 User-Level Object(커널 오브젝트X)
+condition_variable cv;
 
 void Producer()
 {
     while (true)
     {
+        // 1) Lock을 잡고
+        // 2) 공유 변수 값을 수정
+        // 3) Lock을 풀고
+        // 4) 조건변수를 통해 다른 쓰레드에게 통지
         {
             unique_lock<mutex> lock(m);
             q.push(100);
         }
 
-        ::SetEvent(handle);
-        this_thread::sleep_for(100ms);
+        cv.notify_one(); // wait중인 쓰레드중 하나만 깨운다.
     }
 }
 
@@ -191,32 +196,26 @@ void Consumer()
 {
     while (true)
     {
-        ::WaitForSingleObject(handle, INFINITE);
-        // bManualReset을 true로 설정 시: ResetEvent(handle);
-
         unique_lock<mutex> lock(m);
-        if (q.empty() == false)
-        {
-            int32 data = q.front();
-            q.pop();
-            cout << data << endl;
-        }
+        cv.wait(lock, []() { return q.empty() == false; });
+        
+        // 1) Lock을 잡고
+        // 2) 조건 확인
+        // - 만족 O => 코드를 이어서 진행
+        // - 만족 X => Lock을 풀어주고 대기 상태
+
+        int32 data = q.front();
+        q.pop();
+        cout << q.size() << endl;
+        
     }
 }
 
 int main()
 {
-    // 커널 오브젝트
-    // Usage Count (커널 오브젝트 공통)
-    // Signaled(이벤트 발생) / Non-Signaled(이벤트 발생 X) << bool (커널 오브젝트 공통)
-    // Auto / Manual << bool
-    handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL);
-
     thread t1(Producer);
     thread t2(Consumer);
 
     t1.join();
     t2.join();
-
-    ::CloseHandle(handle);
 }
