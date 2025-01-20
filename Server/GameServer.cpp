f67ded9939d742be719d39cb68c074d1a901a8ef
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-01-20 23:53:28 +0900

    🎉 add. 소수 구하기(쓰레드 사용)

--- Server/GameServer.cpp
+++ Server/GameServer.cpp
@@ -15,9 +15,6 @@
 
 #include "Memory.h"
 
-#include "PlayerManager.h"
-#include "AccountManager.h"
-
 //void HandleError(const char* cause)
 //{
 //    int32 errCode = ::WSAGetLastError();
@@ -171,27 +168,64 @@
 //    ::WSACleanup();
 //}
 
+bool isPrime(int number)
+{
+    if (number <= 1)
+        return false;
+    
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+int CountPrime(int start, int end)
+{
+    int count = 0;
+
+    for (int num = start; num <= end; num++)
+    {
+        if (isPrime(num))
+            count++;
+    }
+
+    return count;
+}
+
 int main(void)
 {
-    GThreadManager->Launch([=]
-        {
-            while (true)
-            {
-                cout << "PlayerThenAccount" << endl;
-                GPlayerManager.PlayerThenAccount();
-                this_thread::sleep_for(100ms);
-            }
-        });
+    const int MAX_NUMBER = 1'000'000;
 
-    GThreadManager->Launch([=]
-        {
-            while (true)
-            {
-                cout << "AccountThenPlayer" << endl;
-                GAccountManager.AccountThenPlayer();
-                this_thread::sleep_for(100ms);
-            }
-        });
+    vector<thread> threads;
 
-    GThreadManager->Join();
+    // 1000 = 168
+    // 10'000 = 1229
+    // 1'000'000 = 78498
+
+    int coreCount = thread::hardware_concurrency();
+    int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+    atomic<int> primeCount = 0;
+
+    for (int i = 0; i < coreCount; i++)
+    {
+        int start = (i * jobCount) + 1;
+        int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+
+        threads.push_back(thread([start, end, &primeCount]()
+            {
+                primeCount += CountPrime(start, end);
+            }));
+    }
+
+    for (thread& t : threads)
+        t.join();
+
+    cout << primeCount << endl;
 }
