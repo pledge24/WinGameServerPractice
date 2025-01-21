05136cdf53c529d997a1858826700acf724e0512
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-01-22 02:15:56 +0900

    🎉 add. Memory Pool #1

--- ServerCore/CoreMacro.h
+++ ServerCore/CoreMacro.h
@@ -18,8 +18,8 @@
 ------------------*/
 
 #ifdef _DEBUG
-#define Xalloc(size)        StompAllocator::Alloc(size)
-#define Xrelease(ptr)       StompAllocator::Release(ptr)
+#define Xalloc(size)        PoolAllocator::Alloc(size)
+#define Xrelease(ptr)       PoolAllocator::Release(ptr)
 #else
 #define Xalloc(size)        BaseAllocator::Alloc(size)
 #define Xrelease(ptr)       BaseAllocator::Release(ptr)
