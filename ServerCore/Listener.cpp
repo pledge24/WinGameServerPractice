c5bc2cd6d9200d6e16fe9d969c3a71d7f5e351c1
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-01-24 02:44:11 +0900

    🎉 add. Session #1

--- ServerCore/Listener.cpp
+++ ServerCore/Listener.cpp
@@ -115,10 +115,6 @@ void Listener::ProcessAccept(AcceptEvent* acceptEvent)
     }
 
     session->SetNetAddress(NetAddress(sockAddress));
-
-    cout << "Client Connected!" << endl;
-
-    // TODO
-
+    session->ProcessConnect();
     RegisterAccept(acceptEvent);
 }
