be18e358cfee4b024773b3203799fedf6b710ffa
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-02-04 02:09:48 +0900

    🎉 add. JobQueue2

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -11,19 +11,27 @@
 #include "Job.h"
 #include "Room.h"
 
+void HealByValue(int64 target, int32 value)
+{
+    cout << target << " " << value << endl;
+}
+
+class Knight
+{
+public:
+    void HealMe(int32 value)
+    {
+        cout << "HealMe! " << value << endl;
+    }
+};
+
 int main()
 {
     // TEST JOB
     {
-        // [일감 의뢰 내용] : 1번 유저한테 10만큼 힐을 줘라!
-        // 행동 : Heal
-        // 인자 : 1번 유저, 10이라는 힐량
-        HealJob healJob;
-        healJob._target = 1;
-        healJob._healValue = 10;
+        FuncJob<void, int64, int32> job(HealByValue, 100, 10);
 
-        // 나~중에
-        healJob.Execute();
+        job.Execute();
     }
 
 	ClientPacketHandler::Init();
